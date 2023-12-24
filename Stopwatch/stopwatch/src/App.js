80a3e21c667b364f00a96fc5e2cd9940fb0b1a4e
neha7227 <neha.indoliya27@gmail.com>
2023-12-24 20:36:41 +0530

    stopwatch version1

--- Stopwatch/stopwatch/src/App.js
+++ Stopwatch/stopwatch/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Stopwatch from './components/Stopwatch.jsx';
+// import Stopwatch_1 from './components/Stopwatch_1.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stopwatch/>
+      {/* <Stopwatch_1/> */}
     </div>
   );
 }
