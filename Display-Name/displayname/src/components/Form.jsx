a11f7deb7f48f8e4539f22dfa98c5cdbe4c4227a
neha7227 <neha.indoliya27@gmail.com>
2023-12-26 19:48:18 +0530

    "form bug fix-1"

--- Display-Name/displayname/src/components/Form.jsx
+++ Display-Name/displayname/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "../components/Form.module.css";
 
 export default function Form() {
   const [fullName, setFullName] = useState("");
@@ -15,48 +16,54 @@ export default function Form() {
     e.preventDefault();
     if (firstName && lastName) {
       setFullName(`${firstName} ${lastName}`);
-      setFirstName("");
-      setLastName("");
     }
+
     // else alert("pls fill details");
     // else console.alert("pls fill details")
   };
 
   return (
     <>
-      <h1>Full Name Display</h1>
-      <form>
-        <div>
-          <label>First Name:</label>
-          <input
-            type="text"
-            id="fname"
-            name="firstname"
-            required
-            onChange={handleFirstName}
-          />
-          <br />
-        </div>
-        <div>
-          <label>Last Name:</label>
-          <input
-            type="text"
-            id="lname"
-            name="lastname"
-            required
-            onChange={handleLastName}
-          />
-          <br />
-        </div>
-        <div>
-          {firstName && lastName ? (
-            <input type="submit" value="Submit" onClick={handleSubmit} />
-          ) : (
-            <button>Submit</button>
-          )}
-        </div>
-      </form>
-      <p>Full Name: {fullName}</p>
+      <div className={styles.container}>
+        <h1>Full Name Display</h1>
+        <form>
+          <div>
+            <label>First Name:</label>
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              required
+              onChange={handleFirstName}
+            />
+            <br />
+          </div>
+          <div>
+            <label>Last Name:</label>
+            <input
+              type="text"
+              id="lname"
+              name="lastname"
+              required
+              onChange={handleLastName}
+            />
+            <br />
+          </div>
+          <div>
+            {firstName && lastName ? (
+              <input
+                type="submit"
+                value="Submit"
+                onClick={handleSubmit}
+                className={styles.submitBtn}
+              />
+            ) : (
+              <button className={styles.submitBtn}>Submit</button>
+            )}
+          </div>
+        </form>
+        <p>Full Name: {fullName}</p>
+      </div>
     </>
   );
 }
